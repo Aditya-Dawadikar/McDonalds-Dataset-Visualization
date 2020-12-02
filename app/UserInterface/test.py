64f053466eef7026ebc13f249a395a4f2998b8f9
aditya-dawadikar <adityadawadikar2000@gmail.com>
2020-12-02 21:11:30 +0530

    donut and safe value graph

--- app/UserInterface/test.py
+++ app/UserInterface/test.py
@@ -1,8 +1,11 @@
 import Modules.dataVisualizationModule as dvm
-
+import numpy as np
 obj=dvm.dataVisualization()
 
 x_data=[10,20,8,70]
 y_data=[10,12,9,11]
 Category_Min = ['Calories', 'Total Fat', 'Cholesterol','Sodium', 'Sugars', 'Carbohydrates']
-obj.heatMap()
+l=np.array([1,4,2,5,2,6,1,7,4,7])
+x_labels=[1,4,2,5,2,6,1,7,4,7]
+y_labels=[1,4,2,5,2,6,1,7,4,7]
+obj.test()
